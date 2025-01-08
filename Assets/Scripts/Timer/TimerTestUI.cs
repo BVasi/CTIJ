e3452a2f0d40d12f957beda9629f470965d53d0d
SergiuGugea <sergiu.gugea@student.upt.ro>
2025-01-08 22:06:47 +0200

    Made UI changes

---
+++ Assets/Scripts/Timer/TimerTestUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class TimerTestUI : MonoBehaviour
+{
+    public Timer timer; // Referință la componenta Timer
+
+    void Start()
+    {
+        if (timer != null)
+        {
+            timer.OnTimerEnd += TimerEnded;
+            timer.StartTimerForSeconds(120); // Pornim un timer de 2 minute
+        }
+    }
+
+    private void TimerEnded()
+    {
+        Debug.Log("Timerul UI s-a terminat!");
+    }
+}
